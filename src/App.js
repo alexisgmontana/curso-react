9efd5084fbffbe6543ddf5018be33db1f4e8c8ae
Alexis Montaña <alexisgmontana@gmail.com>
2023-03-04 14:23:29 -0300

    Agregando enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
-import ConsumiendoApi from "./Components/ConsumiendoApi/ConsumiendoApi";
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar";
-import pruebasHook from "./Components/pruebasHook/pruebasHook";
+import Login from "./Components/Login/Login";
+import { RiH1 } from "react-icons/ri";
 
 function App() {
   return (
-    <div>
-      <Navbar>
-        <ItemListContainer greeting={"Cliente"} />
-        <ConsumiendoApi />
-      </Navbar>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element= { <ItemListContainer /> } />
+        <Route path="/login" element= { <Login /> } />
+
+        <Route path="*" element= { <h1> error 404: Not found </h1> } />
+
+      </Routes>
+    </BrowserRouter>    
   );
 }
 
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,8 @@
+
+const Login = () => {
+  return (
+    <div>Estoy en el Login</div>
+  )
+}
+
+export default Login
